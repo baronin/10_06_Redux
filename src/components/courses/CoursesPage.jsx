8790e6323c4e8c48d2a45b36c05ecd2d630544fd
romannikiforov <escaro@ukr.net>
2021-08-27 19:33:19 +0300

    initial commit

---
+++ src/components/courses/CoursesPage.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const initialCourse = {
+  title: "",
+};
+
+const CoursesPage = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Courses Page</h1>
+    </div>
+  );
+};
+
+export default CoursesPage;
